c94e84f588555f05a8b4e3c0f0d100dec2809b0d
phorge <ktrujillosullivan@gmail.com>
2020-04-09 20:55:19 -0600

    first tween pass, lastProgress, progress update for 4/9

--- js/track.js
+++ js/track.js
@@ -7,14 +7,16 @@ const people = [
     {
         name:"Kassi",
         color:"#f7eb05",
-        progress: .61,
+        progress: .72,
+        lastProgress: .61,
         lane: 0,
         path: null
     },
     {
         name:"Denise",
         color:"#ffffff",
-        progress: .39,
+        progress: .61,
+        lastProgress: .39,
         lane: 1,
         path: null
     },
@@ -22,27 +24,31 @@ const people = [
         name:"Dakotah",
         color:"#0c26ed",
         progress: .34,
+        lastProgress: .34,
         lane: 2,
         path: null
     },
     {
         name:"Joe",
         color:"#09ed11",
-        progress: .61,
+        progress: .69,
+        lastProgress: .61,
         lane: 3,
         path: null
     },
     {
         name:"Kelly",
         color:"#830fd1",
-        progress: .60,
+        progress: .67,
+        lastProgress: .60,
         lane: 4,
         path: null
     },
     {
         name:"Todd",
         color:"#f58c0c",
-        progress: .58,
+        progress: .62,
+        lastProgress: .58,
         lane: 5,
         path: null
     }
@@ -159,11 +165,12 @@ function drawMarker (lane0path, person, origin, width, height) {
     var radius = height/2;
     var offset = (person.lane + 1) * 10;
     var startOffset = person.path.length-lane0path.length;
-    var startPoint = person.path.getPointAt(startOffset+lane0path.length*person.progress);
+    var startPoint = person.path.getPointAt(startOffset+lane0path.length*person.lastProgress);
     var marker = new paper.Path.Circle(startPoint, 5);
     marker.fillColor = person.color;
-    var startLine = new paper.Path();
+    var endPoint = person.path.getPointAt(startOffset+lane0path.length*person.progress);
 
+    var startLine = new paper.Path();
     startLine.strokeColor = '#e9e8eb';
     startLine.strokeWidth = 1.75;
     
@@ -176,6 +183,12 @@ function drawMarker (lane0path, person, origin, width, height) {
     //startLine.lineBy(new paper.Point(0,-person.path.strokeWidth));
     
     //startLine.lineTo(new paper.Point(origin.width+radius, origin.height-offset));
+    marker.tween(
+        {position: startPoint},
+        {position: endPoint},
+        3000
+    );
+
 }
   
 function drawLegend (person, fieldOrigin) {
